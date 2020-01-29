e5a6e03b29a18345689b4a2652d683a9600151cb
Luohao Xu <luohaox@gmail.com>
2020-01-29 10:36:54 +0100

    make duplications

--- popGen/gvcf.sh
+++ popGen/gvcf.sh
@@ -10,6 +10,11 @@ sample=$1
 samtools faidx $spe.fa
 /apps/java/1.8u152/bin/java -jar /apps/picard_tools/2.14.0/picard.jar CreateSequenceDictionary R=$spe.fa O=$spe.dict
 
+# make duplications
+java -Xmx108g -jar /apps/picard_tools/2.14.0/picard.jar MarkDuplicates I=$spe.fa.$sample.sorted.bam O=$spe.fa.$sample.dedup.bam M=$size.m
+samtools index $spe.fa.$sample.dedup.bam
+
+
 # gatk
 /apps/java/1.8u152/bin/java -jar /apps/gatk/nightly-2017-12-12/GenomeAnalysisTK.jar  -R $spe.fa -T HaplotypeCaller -ERC GVCF -I $spe.fa.$sample.dedup.bam  -o $sample.g.vcf.gz -nct 4
 
